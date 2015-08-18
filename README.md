fa9a562b006db91e922f76f579201abb0df163e4
om3rcitak <mail@omercitak.com>
2015-08-18 20:45:10 +0300

    ctf questions commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# lyk2015ctf
+Linux Yaz Kampı 2015'te Web Güvenliği sınıfı için hazırlamış olduğum CTF soruları.
---
+++ index.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: om3rcitak
+ */
+?>
+<!doctype html>
+<html>
+<head>
+    <meta charset="utf-8"/>
+    <title>Linux Yaz Kampı 2015 Web Güvenliği CTF</title>
+    <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css"/>
+    <style type="text/css">
+        .well {
+            margin-top: 50px;
+        }
+    </style>
+</head>
+<body>
+<div class="container well">
+    <h2 class="text-center">Linux Yaz Kampı 2015 Web Güvenliği CTF</h2>
+    <ol>
+        <li><a href="web1.php" target="_blank">Login Olamıyorum :(</a></li>
+        <li><a href="matrix.php" target="_blank">Meetrix</a></li>
+        <li><a href="bot.php" target="_blank">Bot</a></li>
+        <li><a href="time.php" target="_blank">Zaman akıp gidiyor</a></li>
+        <li><a href="xss.php" target="_blank">İkseses</a></li>
+        <li><a href="emlak.php" target="_blank">Emlakçı</a></li>
+    </ol>
+    <div class="text-center">
+        <a href="https://twitter.com/search?q=%23lyk2015&src=typd"
+           target="_blank">#lyk2015</a>, <a href="http://omercitak.com/" target="_blank">Ömer
+            Çıtak</a>
+    </div>
+</div>
+</body>
+</html>
